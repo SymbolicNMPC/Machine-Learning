39631a6dd4c04b28c5949c66014922c9e6689128
Behzad Samadi <behzad.samadi@gmail.com>
2018-01-26 14:29:43 -0500

    Updating GP functions

---
+++ src/GaussianProcess/KernelFunction.mm
@@ -0,0 +1,22 @@
+KernelFunction := proc(X1,X2,logtheta,n,N1,N2)
+   local theta::Vector,
+         K::Matrix,
+         V::Vector,
+         i,
+         j,
+         k;
+
+   for i to n+1 do
+      theta[i] := exp(logtheta[i]);
+   end do;
+
+   for i to N1 do
+   	for j to N2 do
+   		for k to n do
+   		   V[k] := (X1[k,i]-X2[k,j])/theta[k];
+   		end do;
+   		K[i,j]:=theta[n+1]*exp(-DotProduct(V,V,n)/2);
+   	end do;
+   end do;
+   return K;
+end proc:
--- src/LinearAlgebra/DotProduct.mm
+++ src/LinearAlgebra/DotProduct.mm
@@ -1,4 +1,10 @@
-DotProduct := proc(x, y)
-   local i;
-   add(x[i]*y[i], i = 1 .. numelems(x));
+CovarianceMatrix := proc(X,logtheta,n,N,CovMat)
+   local theta, i;
+
+   theta := exp(logtheta[n+2]);
+
+   KernelFunction(X,X,logtheta,n,N,N,CovMat);
+   for i to N do
+      CovMat[i,i]:=CovMat[i,i]+theta^2;
+   end do;
 end proc:
